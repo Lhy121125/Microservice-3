077c0eb03b477ecb124165f104f5e196d6e67b6b
Dongchen Zou <53137640+ZouDCh@users.noreply.github.com>
2023-11-14 20:29:11 -0500

    emails?

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from send_email import publish
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello, from Docker!\nWe will use this to deploy application service!"
 
+@app.route("/email")
+async def send_email():
+    publish("dz2506@columbia.edu","test from docker","test email")
+    return {"message":"Success"}
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
---
+++ send_email.py
@@ -0,0 +1,34 @@
+from google.cloud import pubsub_v1
+import json
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./primeval-alloy-383019-66a5b359fa11.json"
+project_id = "primeval-alloy-383019"
+topic_name = "application-status-email"
+
+
+def publish_to_pubsub(project_id, topic_name, message):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id,topic_name)
+
+    message_data = json.dumps(message).encode('utf-8')
+
+    future = publisher.publish(topic_path, data=message_data)
+    print(f"Published message '{message}' to {topic_path}")
+
+    future.result()
+
+def publish(to,subject,body):
+    message = {"to":to,
+               "subject":subject,
+               "body":body}
+    publish_to_pubsub(project_id,topic_name,message)
+
+if __name__=="__main__":
+    message = 	{
+      "to": "dz2506@columbia.edu",
+      "subject": "On python",
+      "body": "This is the email body content."
+    }
+
+    publish_to_pubsub(project_id, topic_name, message)
